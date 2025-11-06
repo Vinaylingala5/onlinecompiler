ca187be40ebc08b330cdcfa04f13d8922923f756
Vinaylingala5 <vinaybabulingala@gmail.com>
2025-11-06 20:12:52 +0530

    feat : auth - logout api

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Users
 from .serializers import UserRegisterSerializer, UserLoginSerializer
@@ -25,3 +25,18 @@ class LoginView(generics.GenericAPIView):
                 'full_name': user.full_name
             }
         }, status=status.HTTP_200_OK)
+
+class LogoutView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            token = RefreshToken(refresh_token)
+
+            token.blacklist()  # blacklist the token so it can’t be reused
+            return Response({"message": "Logout successful."}, status=status.HTTP_205_RESET_CONTENT)
+        except TokenError:
+            return Response({"error": "Invalid or expired token."}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return Response({"error": "Something went wrong."}, status=status.HTTP_400_BAD_REQUEST)
